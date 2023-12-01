1ccb2271ca397a435bf529c2bea85e8f0f3bb01c
AlexJMcIntyre <66202153+AlexJMcIntyre@users.noreply.github.com>
2023-12-01 22:55:54 +0000

    make neat

--- 1b.py
+++ 1b.py
@@ -1,17 +1,15 @@
 f = open("1a_real.txt")
 print(type(f))
 
-r_list = [("one", "o1ne"), ("two", "t2wo"), ("three", "t3hree"), ("four", "f4our"), ("five", "f5ive"), ("six", "s6ix"), ("seven", "s7even"),
-          ("eight", "e8ight"), ("nine", "n9ine")]
+r_list = [("one", "o1e"), ("two", "t2o"), ("three", "t3e"), ("four", "f4r"), ("five", "f5e"), ("six", "s6x"), ("seven", "s7n"),
+          ("eight", "e8t"), ("nine", "n9e")]
 
 fl = f.readlines()
 
 sol = []
 for l in fl:
-    print(l)
     for r in r_list:
         l = l.replace(r[0], r[1])
-    print(l)
 
     a1 = ''
     a2 = ''
